d0471325227c926b3e7869974d1eae0d7806caba
Ben West <bwest@healthefilings.com>
2018-02-27 10:58:19 -0600

    working database

--- db/migrate/20180227165122_create_orders.rb
+++ db/migrate/20180227165122_create_orders.rb
@@ -1,12 +1,12 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.text exchange
-      t.text side
-      t.text buy_currency
-      t.text sell_currency
-      t.real price
-      t.real quantity
+      t.text :exchange
+      t.text :side
+      t.text :buy_currency
+      t.text :sell_currency
+      t.decimal :price
+      t.decimal :quantity
 
       t.index %i[buy_currency sell_currency side]
     end
